96a43a7284eb2454c83080a6e2c1b549bdbb6413
AleksandrAfanasev <jgBeM9bkX@@hEq3>
2023-02-18 21:40:53 +0300

    Third stage

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,23 +1,21 @@
 import json
-import re
 
 x = input().strip(' ')
 data = json.loads(x)
-errors = {'bus_id': 0,
-'stop_id': 0,
-'stop_name': 0,
-'next_stop': 0,
-'stop_type': 0,
-'a_time': 0}
+Bus_line_info = {'bus_id: 128': 0,
+'bus_id: 256': 0,
+'bus_id: 512': 0,
+'bus_id: 1024': 0}
 for element in data:
-    if re.match(r'(S|O|F|)$', element.get("stop_type")) is None:
-        errors["stop_type"] += 1
-    if re.match(r'([A-Z]\w* )+(Street|Avenue|Boulevard|Road)$', element.get("stop_name")) is None:
-        errors["stop_name"] += 1
-    if re.match(r'[021]{1}[0-9]{1}:[0-5]{1}[0-9]{1}$', element.get("a_time")) is None:
-        errors["a_time"] += 1
+    if element.get("bus_id") == 128:
+        Bus_line_info["bus_id: 128"] += 1
+    if element.get("bus_id") == 256:
+        Bus_line_info["bus_id: 256"] += 1
+    if element.get("bus_id") == 512:
+        Bus_line_info["bus_id: 512"] += 1
+    if element.get("bus_id") == 1024:
+        Bus_line_info["bus_id: 1024"] += 1
 
-print(f'Format validation: {sum(errors.values())} errors')
-print('stop_name:', errors.get("stop_name"))
-print('stop_type:', errors.get("stop_type"))
-print('a_time:', errors.get("a_time"))
+print('Line names and number of stops:')
+for element in Bus_line_info:
+    print(element+',', 'stops:', Bus_line_info[element])
